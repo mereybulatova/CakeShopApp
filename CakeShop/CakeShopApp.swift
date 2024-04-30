0dae2994fbeb5cfd5a4ed8130942215d03717ec1
Merey <mereybulatova05@gmail.com>
2024-04-30 20:15:47 +0500

    Initial Commit

---
+++ CakeShop/CakeShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  CakeShopApp.swift
+//  CakeShop
+//
+//  Created by Мерей Булатова on 30.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct CakeShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
